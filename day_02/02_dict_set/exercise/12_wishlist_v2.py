0b16efd73be86b84d0916b0972e9afcd86d11291
Nelrruiz <nelicaruiz@gmail.com>
2025-11-09 16:01:03 +0800

    day 2 early commit

--- day_02/02_dict_set/exercise/12_wishlist_v2.py
+++ day_02/02_dict_set/exercise/12_wishlist_v2.py
@@ -1,8 +1,24 @@
 # TODO: Fill in the details of the items you plan to buy
-order = {
-    "Name": ...,
-    "Info": ...,
-}
+wishlist = [
+    {
+    	"Name": "Toblerone",
+    	"Info": "Dark Chocolate",
+    	"Price": 85,
+    	"Weight": "100g"
+	},
+    {
+    	"Name": "Oreo",
+    	"Info": "Double Stuff",
+    	"Price": 50,
+    	"Weight": "150g"
+	},
+    {
+    	"Name": "Chips Ahoy",
+    	"Info": "Original Chocolate Chip",
+    	"Price": 25,
+    	"Weight": "38g"
+	}
+]
 
 # TODO: Print the item details in the following format (for each order):
 """
@@ -11,3 +27,7 @@ Order:
 	Info: item info
 	...
 """
+for order in wishlist:
+	print("Order:")
+	for field, details in order.items():
+		print(f"\t{field}: {details}")
