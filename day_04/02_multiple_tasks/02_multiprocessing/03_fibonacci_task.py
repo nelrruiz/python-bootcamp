64fa51fd6210e505365ae3426532654454da725a
bomacs <eymardfaderon@gmail.com>
2025-11-16 12:55:59 +0800

    day 4 first half commit

--- day_04/02_multiple_tasks/02_multiprocessing/03_fibonacci_task.py
+++ day_04/02_multiple_tasks/02_multiprocessing/03_fibonacci_task.py
@@ -1,15 +1,25 @@
 import cProfile
+from multiprocessing import Pool
+# from functools import cache
 
+# @cache
 def fib(n):
     if n <= 1:
         return n
     return fib(n - 1) + fib(n - 2)
 
-
+# multiprocessing == 12 secs
 def main():
     inputs = [35, 36, 37, 38]
-    outputs = [fib(number) for number in inputs]
+    # outputs = [fib(number) for number in inputs]
+    with Pool() as pool:
+        outputs = pool.map(fib, inputs)
 
+# # cache method == 0 secs
+# def main():
+#     inputs = [35, 36, 37, 38]
+#     for input in inputs:
+#         fib(input)
 
 if __name__ == '__main__':
     cProfile.run("main()", sort="cumtime")
